7df3bb6bb8bb2f2f7ff4016effd001832633937b
Fidi Arya <fidiarya@gmail.com>
2024-08-01 10:05:07 +0700

    update fix error

--- sdt_udf_sbk/models/account_move.py
+++ sdt_udf_sbk/models/account_move.py
@@ -15,10 +15,11 @@ class AccountMove(models.Model):
     def _compute_payment_date(self):
         for data in self:
             payment_date =  False
-            reconciled_partials = data._get_all_reconciled_invoice_partials()
-            if reconciled_partials:
-                aml_ids = [i['aml'].id for i in reconciled_partials]
-                payments = self.env['account.move.line'].browse(aml_ids).mapped('payment_id') 
-                if payments:
-                    payment_date = sorted(payments, key=lambda x: x['date'], reverse=True)[0]['date']
+            if data._origin.id and data.move_type != 'entry':
+                reconciled_partials = data._get_all_reconciled_invoice_partials()
+                if reconciled_partials:
+                    aml_ids = [i['aml'].id for i in reconciled_partials]
+                    payments = self.env['account.move.line'].browse(aml_ids).mapped('payment_id') 
+                    if payments:
+                        payment_date = sorted(payments, key=lambda x: x['date'], reverse=True)[0]['date']
             data.payment_date = payment_date
